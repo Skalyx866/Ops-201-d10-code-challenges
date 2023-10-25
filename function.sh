631cce5d77d5b5007d2ca63bcc4da922246d3602
Cody <skalyx86@gmail.com>
2023-10-25 12:17:50 -0700

    Ops challenge 3

---
+++ function.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script Name:                  login tracker
+# Author:                       Cody Juhl
+# Date of latest revision:      10/25/2023
+# Purpose:                      
+
+# Declaration of variables
+prompt="This is the login history!"
+# Declaration of functions
+login-history()
+{
+    last
+    echo "$prompt"
+    echo
+}
+
+# Main
+login-history
+
+login-history
+
+login-history
+
+# End
