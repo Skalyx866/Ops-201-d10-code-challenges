841b4a021a34560bc315a8ef4fb7d338e0400931
Cody Juhl <147422815+Skalyx866@users.noreply.github.com>
2023-11-06 23:25:05 -0800

    Add files via upload

---
+++ Ops201-challenge 10.ps1
@@ -0,0 +1,17 @@
+﻿Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+#Goes into the firewall rules under file and printer sharing and enables
+
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 
+#Creates a new firewall inbound rule that allows ICMP
+
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+#Edits the registry to turn fdenytsconnections to false
+
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+#Grabs the debloater from github and executes it
+
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+#checks windows optional features and gets the Hyper-V components and installs
+
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+#Sets the SMBv1 protocol to false and forces it
