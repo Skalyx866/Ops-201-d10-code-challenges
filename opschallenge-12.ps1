929660ca42618ec36e929a9dd7d53589efb15296
Cody Juhl <147422815+Skalyx866@users.noreply.github.com>
2023-11-07 20:50:15 -0800

    Add files via upload

---
+++ opschallenge-12.ps1
@@ -0,0 +1,6 @@
+﻿$path = "C:\Users\CodyR\Documents\network-report.txt"
+#setting a variable to set the path to the textfile network-report.txt
+
+function get-ipv4 {Select-String -Path $path -Pattern "ipv4"}
+#creating a function using the select screen and using the path variable to set the path to the network-report.txt and using select-string to grabe the line that says ipv4
+get-ipv4
