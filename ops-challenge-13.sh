76e5a611644ac00fb6589b107674e71c3a173f32
Cody <skalyx86@gmail.com>
2023-11-08 12:15:59 -0800

    added comments

--- ops-challenge-13.sh
+++ ops-challenge-13.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-commands=( whois dig host nslookup )
-echo "Please type in a domain"
-read domain
-dns-records ()
+commands=( whois dig host nslookup ) #commands to be used by the script
+echo "Please type in a domain" #prompting the user to enter a domain name
+read domain #read the user input and put it the domain variable
+dns-records () 
 {
     
         for command in ${commands[*]}
@@ -11,4 +11,5 @@ dns-records ()
             $command $domain >> 'dns-records.txt'
         done
 }
-dns-records
+#Using a for loop using the array of commands to run against the domain variable and executing the commands, putting it in a text file called dns-records.txt
+dns-records #call the function
